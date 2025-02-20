7d0b4a3c2b9ebdafe97986c0fec32a4d434ad0bd
Gil-hq15 <146899416+Gil-hq15@users.noreply.github.com>
2025-02-20 17:29:15 -0600

    Update check_connections.sh

--- scripts/check_connections.sh
+++ scripts/check_connections.sh
@@ -1,14 +1,14 @@
 # Probando PostgreSQL
 # pg_isready -h localhost -p 5432
 echo "Testing PostgreSQL..."
-docker exec -it lab_2-postgres-1 pg_isready -h localhost -p 5432
+docker exec -it Big-Data-Environment-postgres-1 pg_isready -h localhost -p 5432
 
 # Probando MongoDB
 #echo 'db.runCommand({ping:1})' | mongosh --host localhost:27017
 echo "Testing MongoDB..."
-docker exec -it lab_2-mongodb-1 mongosh --host localhost:27017 --eval 'db.runCommand({ping:1})'
+docker exec -it Big-Data-Environment-mongodb-1 mongosh --host localhost:27017 --eval 'db.runCommand({ping:1})'
 
 # Probando Redis
 # redis-cli -h localhost ping
 echo "Testing Redis..."
-docker exec -it lab_2-redis-1 redis-cli -h localhost ping
+docker exec -it Big-Data-Environment-redis-1 redis-cli -h localhost ping
